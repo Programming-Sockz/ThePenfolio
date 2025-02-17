668cf4ef847d8fc01744a12112ad33187c143144
Thomas Kind <thomas.kind@softtec.de>
2025-02-17 14:23:44 +0100

    test controller and frontend project init

---
+++ Server/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThePenfolio.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            await Task.Delay(1);
+            return "Hello from the server!";
+        }
+    }
+}
---
+++ Shared/libraries/ApiRoutes.cs
@@ -0,0 +1,13 @@
+﻿namespace ThePenfolio.Shared.libraries
+{
+    public static class ApiRoutes
+    {
+        public static class Test
+        {
+            private const string _base = "api/test";
+            
+            public static string Get() 
+            => $"{_base}";
+        }
+    }
+}
