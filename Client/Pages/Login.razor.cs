8776939bafa2268375cab5cc3e7baa48c31ce815
Moritz Starke <starkemoritz@web.de>
2025-02-19 21:31:12 +0100

    Feat: Basic login page (`razor.cs` file currently has logic in function)

---
+++ Client/Pages/Login.razor.cs
@@ -0,0 +1,25 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using ThePenfolio.Shared.DTOs;
+using System.Net.Http.Json;
+using ThePenfolio.Shared.libraries;
+
+namespace ThePenfolio.Client.Pages
+{
+    public partial class Login
+    {
+        [Inject] public HttpClient Http { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        [Inject] public ILocalStorageService LocalStorageService { get; set; }
+
+        private string loginStorageKey = "loginStamp";
+        private LoginRequestDTO login = new();
+        private LoginResponseDTO? responseDTO;
+        private bool isSubmitting = false;
+
+        private async Task ValidSubmit()
+        {
+    
+        }
+    }
+}
